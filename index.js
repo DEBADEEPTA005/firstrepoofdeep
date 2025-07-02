454d870bc829af495a77c0b129ad1c04465fe871
DEBADEEPTA005 <welcomedebadeepta@gmail.com>
2025-07-02 22:07:17 +0530

    Auto commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.log(`Hello`);
+//console.log(`I like pizzas`);
+//window.alert(`This is an alert`);
+//document.getElementById("myH1").textContent =`Hello`;
+//document.getElementById("myP").textContent =`I like pizza`;
+let x=100;
+let age=10;
+console.log(x);
+console.log(`You are $`);
